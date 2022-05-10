2ee34bb730a6baccdd4a3b8dd9f5e295653538c2
chhotu marandi <cmarandi29@gmail.com>
2022-05-10 12:00:33 +0530

    javascript add object

---
+++ script.js
@@ -0,0 +1,22 @@
+//people profile data 
+const review = {
+    id: 1,
+    name: "jeneliya oslen",
+    job: "web developer",
+    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    text: "he whole idea of working in a learning atmosphere (since it is a university), makes you feel young and gives the opportunity to interact and network with bright minds (Professors/Researchers). Work environment is amazing and I have a very supportive team",
+},
+{
+    id: 2,
+    name: "mark jones",
+    job: "web developer",
+    img: "https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    text: "I enjoyed the work which was challenging and eventually made me a much better developer than when I started. The culture is ideal for those who prize work-life balance. Deadlines are serious but management is realistic and respectful of our time and effort. I felt 100% supported throughout my time there.",
+},
+{
+    id: 3,
+    name: "mark jones",
+    job: "web developer",
+    img: "https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    text: "I enjoyed the work which was challenging and eventually made me a much better developer than when I started. The culture is ideal for those who prize work-life balance. Deadlines are serious but management is realistic and respectful of our time and effort. I felt 100% supported throughout my time there."
+}
