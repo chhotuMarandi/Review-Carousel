62dec93b35533c2c8f3a92c38d98f80f0696e0f9
chhotu marandi <cmarandi29@gmail.com>
2022-05-10 12:19:09 +0530

    select items

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 //people profile data 
-const review = {
-    id: 1,
-    name: "jeneliya oslen",
-    job: "web developer",
-    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-    text: "he whole idea of working in a learning atmosphere (since it is a university), makes you feel young and gives the opportunity to interact and network with bright minds (Professors/Researchers). Work environment is amazing and I have a very supportive team",
-},
-{
-    id: 2,
-    name: "mark jones",
-    job: "web developer",
-    img: "https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
-    text: "I enjoyed the work which was challenging and eventually made me a much better developer than when I started. The culture is ideal for those who prize work-life balance. Deadlines are serious but management is realistic and respectful of our time and effort. I felt 100% supported throughout my time there.",
-},
-{
-    id: 3,
-    name: "mark jones",
-    job: "web developer",
-    img: "https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
-    text: "I enjoyed the work which was challenging and eventually made me a much better developer than when I started. The culture is ideal for those who prize work-life balance. Deadlines are serious but management is realistic and respectful of our time and effort. I felt 100% supported throughout my time there."
-}
+const review = [
+    {
+        id: 1,
+        name: "jeneliya oslen",
+        job: "web developer",
+        img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        text: "he whole idea of working in a learning atmosphere (since it is a university), makes you feel young and gives the opportunity to interact and network with bright minds (Professors/Researchers). Work environment is amazing and I have a very supportive team",
+    },
+    {
+        id: 2,
+        name: "mark jones",
+        job: "web developer",
+        img: "https://images.unsplash.com/photo-1504199367641-aba8151af406?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        text: "1. Scope of skill improvement - A huge scope of developing own skill as there are many different types of projects. 2. Good environment - Very good and friendly working environment. 3. Co-operative seniors - Seniors are very helpful and supportive. 4. Work independence. 5. No undue work pressure. 6. Very good appraisal.",
+    },
+    {
+        id: 3,
+        name: "yuan lee",
+        job: "web developer",
+        img: "https://images.unsplash.com/photo-1544168190-79c17527004f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60s",
+        text: "I have been working at Toppscholars for almost a year and to be honest this is a very good organization. I was unsure about working here before considering that it’s far away from my home but it’s worth it. The colleagues are very helpful and easy to talk to. I’m glad I have the opportunity to work here and develop my skills.",
+    },
+    {
+        id: 4,
+        name: "natasha carel",
+        job: "web developer",
+        img: "https://images.unsplash.com/photo-1542596768-5d1d21f1cf98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
+        text: "Work from home is so nice to have, this is company is highly professional. Gives lots of benefits and opportunity to grow to its employees. I have recetnly started in this company and i think i will have a good and long career ahead."
+    }
+    
+    
+];
+
+//select itemes
+
+const img = document.getElementById("person-img");
+const name = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
